6246b20f9aa0eac368542f10aa85631512d7ad40
unknown <nickarsenal007@gmail.com>
2019-12-01 11:03:26 +0545

    Render first component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Contact from './components/Contact';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>The App Components</h1>
+      	<h1>The App Components</h1>
+ 		<Contact />
     </div>
   );
 }
